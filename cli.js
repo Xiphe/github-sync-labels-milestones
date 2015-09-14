14677196aab5e56affe0761242cab28faa07b080
Xiphe <github@xiphe.net>
2015-09-14 21:18:55 +0200

    feat: implement that thing

--- cli.js
+++ cli.js
@@ -2,4 +2,40 @@
 
 'use strict';
 
-console.log('hello');
+console.log('WIP');
+require('./lib/index')({
+  token: '...',
+  log: console
+}, [{
+  'repositories': [
+    'Xiphe/test'
+  ],
+  'milestones': [
+    {
+      'previousTitles': [
+        '[BG-Area] Internal Release',
+        'hallo'
+      ],
+      'title': 'liufgyiofjopi',
+      'state': 'open',
+      'description': 'Hallo Welt',
+      'due_on': '2015-09-04T23:59:59Z'
+    }, {
+      'title': 'Banana',
+      'state': 'absent'
+    }
+  ],
+  'labels': [
+    {
+      'previousNames': [
+        'krass'
+      ],
+      'name': 'Hallo',
+      'color': 'ff0000'
+    }
+  ]
+}]).then(function() {
+  console.log('ok');
+}, function(err) {
+  console.log('error', err);
+});
