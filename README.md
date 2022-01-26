1dff360a503ea2a6d906ecdaea99365775d3a429
Xiphe <github@xiphe.net>
2022-01-26 23:41:03 +0100

    feat: update to octokit
    this updates all packages and moves github client to https://octokit.github.io/rest.js/v18/
    
    also now supports description for labels
    
    fix https://github.com/Xiphe/github-sync-labels-milestones/issues/19
    fix https://github.com/Xiphe/github-sync-labels-milestones/issues/18

--- README.md
+++ README.md
@@ -1,50 +1,40 @@
-github-sync-labels-milestones
------------------------------
+## github-sync-labels-milestones
 
 Sync Labels and Milestones across Multiple repositories
 
-Install
--------
+## Install
 
 `npm install -g github-sync-labels-milestones`
 
-
-CLI usage
----------
+## CLI usage
 
 `github-sync-labels-milestones <options>`
 
 ### Options
 
-  - `-t, --token`  
-    [required] [github personal access token with repo access](https://github.com/settings/tokens/new)
-  - `-c, --config`  
-    [required] path to config file
-  - `-v, --verbose`  
-    make output more verbose
-  - `-s, --silent`  
-    oppress output
-  - `-V, --version`  
-    output version
-  - `--host`  
-    set github host (see [node-github](https://github.com/mikedeboer/node-github#example))
-  - `--path-prefix`  
-    set github path prefix (see [node-github](https://github.com/mikedeboer/node-github#example))
-  - `--timeout`  
-    set github request timeout (see [node-github](https://github.com/mikedeboer/node-github#example))
-  - `--disable-follow`  
-    disable redirects (sometimes causes issues with GitHub API)
-  - `-h, --help`  
-    output help message
-  - `--no-color`  
-    disable colors
+- `-t, --token`  
+  [required] [github personal access token with repo access](https://github.com/settings/tokens/new)
+- `-c, --config`  
+  [required] path to config file
+- `-v, --verbose`  
+  make output more verbose
+- `-s, --silent`  
+  oppress output
+- `-V, --version`  
+  output version
+- `--baseUrl`  
+  sset github baseUrl (see [node-github](https://github.com/mikedeboer/node-github#example))
+- `--timeout`  
+  set github request timeout (see [node-github](https://github.com/mikedeboer/node-github#example))
+- `-h, --help`  
+  output help message
+- `--no-color`  
+  disable colors
 
 Get a personal access token under [settings/tokens](https://github.com/settings/tokens)
 and ensure [repo] and [public_repo] scopes are activated
 
-
-Configuration File
-------------------
+## Configuration File
 
 The configuration file needs to be valid JSON or YAML and describe
 an array of configuration objects.
@@ -52,124 +42,121 @@ an array of configuration objects.
 ### Example (`config.json`)
 
 ```json
-[{
-  "repositories": [
-    "Jimdo/github-sync-labels-milestones",
-    "Xiphe/example"
-  ],
-  "milestones": [{
-    "previousTitles": [
-      "Complete Everything"
+[
+  {
+    "repositories": ["Jimdo/github-sync-labels-milestones", "Xiphe/example"],
+    "milestones": [
+      {
+        "previousTitles": ["Complete Everything"],
+        "title": "First Release",
+        "state": "open",
+        "description": "Tasks to be completed before release",
+        "due_on": "2015-09-15T23:59:59Z"
+      }
     ],
-    "title": "First Release",
-    "state": "open",
-    "description": "Tasks to be completed before release",
-    "due_on": "2015-09-15T23:59:59Z"
-  }],
-  "labels": [{
-    "previousNames": [
-      "bug"
+    "labels": [
+      {
+        "previousNames": ["bug"],
+        "name": "type: bug",
+        "color": "fc2929"
+      },
+      {
+        "name": "type: feature request",
+        "state": "absent"
+      }
     ],
-    "name": "type: bug",
-    "color": "fc2929"
-  }, {
-    "name": "type: feature request",
-    "state": "absent"
-  }],
-  "options": {
-    "ignoreLabels": [
-      "Xiphe/example"
-    ]
+    "options": {
+      "ignoreLabels": ["Xiphe/example"]
+    }
   }
-}]
+]
 ```
 
 ### configuration object shape
 
-  - #### `repositories`
+- #### `repositories`
 
-    Array of repositories on which the labels and milestones
-    should be synchronized
+  Array of repositories on which the labels and milestones
+  should be synchronized
 
-  - #### `milestones`
+- #### `milestones`
 
-    Array of milestones that should be synchronized
+  Array of milestones that should be synchronized
 
-  - #### `labels`
+- #### `labels`
 
-    _optional_ Array of labels that should be synchronized
+  _optional_ Array of labels that should be synchronized
 
-  - #### `options`
+- #### `options`
 
-    _optional_ Options
-
+  _optional_ Options
 
 ### milestone declaration shape
 
-  - #### `title`
+- #### `title`
 
-    The title of the milestone
+  The title of the milestone
 
-  - #### `previousTitles`
+- #### `previousTitles`
 
-    _optional_ Array of titles this milestone had before.
-    We will try to __rename__ milestones found here to `title`
-    instead of creating a new one
+  _optional_ Array of titles this milestone had before.
+  We will try to **rename** milestones found here to `title`
+  instead of creating a new one
 
-  - #### `state`
+- #### `state`
 
-    _optional_ The state of the milestone. Either `open`, `closed` or `absent`.   
-    Default: `open`  
+  _optional_ The state of the milestone. Either `open`, `closed` or `absent`.  
+  Default: `open`
 
-  - #### `description`
+- #### `description`
 
-    _optional_ A description of the milestone
+  _optional_ A description of the milestone
 
-  - #### `due_on`
+- #### `due_on`
 
-    _optional_ The milestone due date. This is a timestamp in ISO 8601 format:  
-    `YYYY-MM-DDTHH:MM:SSZ`
-
+  _optional_ The milestone due date. This is a timestamp in ISO 8601 format:  
+  `YYYY-MM-DDTHH:MM:SSZ`
 
 ### label declaration shape
 
-  - #### `name`
+- #### `name`
 
-    The name of the label
+  The name of the label
 
-  - #### `color`
+- #### `color`
 
-    A 6 character hex code, without the leading #, identifying the color
+  _optional_ A 6 character hex code, without the leading #, identifying the color
 
-  - #### `previousNames`
+- #### `description`
 
-    _optional_ Array of names this label had before.
-    The new label will be added to issues with any of these
-    and the previous labels will then be deleted.
+  _optional_ description for the label
 
-  - #### `state`
+- #### `previousNames`
 
-    _optional_ Can be `present` or `absent`.   
-    Default: `present`  
+  _optional_ Array of names this label had before.
+  The new label will be added to issues with any of these
+  and the previous labels will then be deleted.
+
+- #### `state`
+
+  _optional_ Can be `present` or `absent`.  
+  Default: `present`
 
 ### Options
 
-  - #### `ignoreLabels`
+- #### `ignoreLabels`
 
-    _optional_ Array of repositories whose labels should not be touched
+  _optional_ Array of repositories whose labels should not be touched
 
+- #### `ignoreMilestones`
 
-  - #### `ignoreMilestones`
+  _optional_ Array of repositories whose milestones should not be touched
 
-    _optional_ Array of repositories whose milestones should not be touched
-
-
-LICENSE
--------
+## LICENSE
 
 > The MIT License
 >
-> Copyright (c) 2017 Hannes Diercks  
+> Copyright (c) 2017 - 2022 Hannes Diercks  
 > Copyright (c) 2015 - 2016 Jimdo GmbH
 >
 > Permission is hereby granted, free of charge, to any person obtaining a copy
--- cli.js
+++ cli.js
@@ -4,23 +4,23 @@
 
 var argv = require('minimist')(process.argv.slice(2), {
   alias: {
-    'token': 't',
-    'config': 'c',
-    'help': 'h',
-    'verbose': 'v',
-    'version': 'V',
-    'silent': 's',
-    'path-prefix': 'pathPrefix'
-  }
+    token: 't',
+    config: 'c',
+    help: 'h',
+    verbose: 'v',
+    version: 'V',
+    silent: 's',
+    'path-prefix': 'pathPrefix',
+  },
 });
-var chalk = require('chalk');
+var Chalk = require('chalk').Instance;
 var Q = require('q');
 var path = require('path');
 var _ = require('lodash');
 var CONSTANTS = require('./lib/constants');
 var YAML = require('yamljs');
 
-chalk = new chalk.constructor({enabled: argv.color});
+var chalk = new Chalk({ level: argv.color === false ? 0 : undefined });
 
 function wrt(str) {
   if (!argv.silent) {
@@ -34,31 +34,44 @@ if (argv.version) {
 }
 
 if (argv.help) {
-  wrt([
-    '',
-    'Usage:',
-    '',
-    '  ' + chalk.cyan('github-sync-labels-milestones') + ' <options>',
-    '',
-    'Options:',
-    '',
-    '  ' + chalk.yellow('-t, --token') + '      [required] github personal access token',
-    '  ' + chalk.yellow('-c, --config') + '     [required] path to config file',
-    '  ' + chalk.yellow('-V, --verbose') + '    make output more verbose',
-    '  ' + chalk.yellow('-s, --silent') + '     oppress output',
-    '  ' + chalk.yellow('-v, --version') + '    output version',
-    '  ' + chalk.yellow('-h, --help') + '       output help message',
-    '  ' + chalk.yellow('--host') + '           set github host ' + chalk.cyan('*'),
-    '  ' + chalk.yellow('--path-prefix') + '    set github path prefix ' + chalk.cyan('*'),
-    '  ' + chalk.yellow('--timeout') + '        set github request timeout ' + chalk.cyan('*'),
-    '  ' + chalk.yellow('--disable-follow') + ' disable redirects (sometimes causes issues with GitHub API)',
-    '  ' + chalk.yellow('--no-color') + '       disable colors',
-    '',
-    chalk.gray('Get a personal access token here: https://github.com/settings/tokens'),
-    chalk.gray('[repo] and [public_repo] scopes need to be activated'),
-    '',
-    chalk.cyan('*') + ' see https://github.com/mikedeboer/node-github#example',
-  ].join('\n') + '\n');
+  wrt(
+    [
+      '',
+      'Usage:',
+      '',
+      '  ' + chalk.cyan('github-sync-labels-milestones') + ' <options>',
+      '',
+      'Options:',
+      '',
+      '  ' +
+        chalk.yellow('-t, --token') +
+        '      [required] github personal access token',
+      '  ' +
+        chalk.yellow('-c, --config') +
+        '     [required] path to config file',
+      '  ' + chalk.yellow('-V, --verbose') + '    make output more verbose',
+      '  ' + chalk.yellow('-s, --silent') + '     oppress output',
+      '  ' + chalk.yellow('-v, --version') + '    output version',
+      '  ' + chalk.yellow('-h, --help') + '       output help message',
+      '  ' +
+        chalk.yellow('--baseUrl') +
+        '        set github baseUrl ' +
+        chalk.cyan('*'),
+      '  ' +
+        chalk.yellow('--timeout') +
+        '        set github request timeout ' +
+        chalk.cyan('*'),
+      '  ' + chalk.yellow('--no-color') + '       disable colors',
+      '',
+      chalk.gray(
+        'Get a personal access token here: https://github.com/settings/tokens',
+      ),
+      chalk.gray('[repo] and [public_repo] scopes need to be activated'),
+      '',
+      chalk.cyan('*') +
+        ' see https://github.com/mikedeboer/node-github#example',
+    ].join('\n') + '\n',
+  );
 
   return;
 }
@@ -83,7 +96,7 @@ function logger(config) {
   }
   logs[config.type][config.state].push(config);
 }
-logger.verbose = function() {
+logger.verbose = function () {
   if (argv.verbose) {
     wrt([].join.call(arguments, ' ') + '\n');
   }
@@ -103,64 +116,76 @@ function getConfig() {
   }
 }
 
-Q.fcall(getConfig).then(function(config) {
-  return require('./lib/index')(
-    {
-      token: argv.token,
-      log: logger,
-    },
-    config,
-    {
-      followRedirects: !argv['disable-follow'],
-      host: argv.host,
-      pathPrefix: argv.pathPrefix,
-      timeout: argv.timeout
-    }
-  );
-}).then(function() {
-  _.forEach(logs, function(states, type) {
-    wrt(chalk.grey(type + ': '));
-    if (argv.verbose) {
-      wrt('\n');
-    }
-    var str = [];
-    _.forEach(states, function(messages, state) {
-      var color;
-      switch (state) {
-        case CONSTANTS.LOG_TYPE_OK:
-          color = chalk.green;
-          break;
-        case CONSTANTS.LOG_TYPE_DELETE:
-          color = chalk.yellow;
-          break;
-        case CONSTANTS.LOG_TYPE_CREATE:
-          color = chalk.cyan;
-          break;
-        case CONSTANTS.LOG_TYPE_UPDATE:
-          color = chalk.magenta;
-          break;
-      }
+Q.fcall(getConfig)
+  .then(function (config) {
+    return require('./lib/index')(
+      {
+        token: argv.token,
+        log: logger,
+      },
+      config,
+      {
+        baseUrl: argv.baseUrl,
+        request: {
+          timeout: argv.timeout,
+        },
+      },
+    );
+  })
+  .then(function () {
+    _.forEach(logs, function (states, type) {
+      wrt(chalk.grey(type + ': '));
       if (argv.verbose) {
-        messages.forEach(function(message) {
-          wrt('  ' + color(state) + ': ' + message.user + '/' +
-            message.repo + ' - ' + message.title + '\n');
-        });
-      } else {
-        str.push(color(messages.length + ' ' + state));
+        wrt('\n');
+      }
+      var str = [];
+      _.forEach(states, function (messages, state) {
+        var color;
+        switch (state) {
+          case CONSTANTS.LOG_TYPE_OK:
+            color = chalk.green;
+            break;
+          case CONSTANTS.LOG_TYPE_DELETE:
+            color = chalk.yellow;
+            break;
+          case CONSTANTS.LOG_TYPE_CREATE:
+            color = chalk.cyan;
+            break;
+          case CONSTANTS.LOG_TYPE_UPDATE:
+            color = chalk.magenta;
+            break;
+        }
+        if (argv.verbose) {
+          messages.forEach(function (message) {
+            wrt(
+              '  ' +
+                color(state) +
+                ': ' +
+                message.user +
+                '/' +
+                message.repo +
+                ' - ' +
+                message.title +
+                '\n',
+            );
+          });
+        } else {
+          str.push(color(messages.length + ' ' + state));
+        }
+      });
+      if (str.length) {
+        wrt(str.join(' ') + '\n');
       }
     });
-    if (str.length) {
-      wrt(str.join(' ') + '\n');
-    }
-  });
 
-  if (argv.verbose) {
-    wrt('\n' + chalk.green('DONE') + '\n');
-  }
-}).catch(function(err) {
-  wrt(chalk.red(err) + '\n');
-  if (argv.verbose) {
-    wrt(err.stack + '\n');
-  }
-  process.exit(1);
-});
+    if (argv.verbose) {
+      wrt('\n' + chalk.green('DONE') + '\n');
+    }
+  })
+  .catch(function (err) {
+    wrt(chalk.red(err) + '\n');
+    if (argv.verbose) {
+      wrt(err.stack + '\n');
+    }
+    process.exit(1);
+  });
